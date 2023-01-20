4cd5349e12bf0f70446549093ff2c98ee592b654
egooktafanda97 <egooktafanda1097@gmail.com>
2023-01-21 03:52:11 +0700

    p

--- system/app/Http/Controllers/Auth/LoginController.php
+++ system/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
@@ -16,7 +17,6 @@ class LoginController extends Controller
     protected $redirectTo = RouteServiceProvider::HOME;
     public function __construct()
     {
-        // $this->middleware('guest')->except('logout');
     }
 
     protected function login(Request $request)
@@ -41,10 +41,22 @@ class LoginController extends Controller
         $genTokenApi = $this->respondWithToken($token);
         \Session::put('token', $genTokenApi);
 
-
-        return redirect($this->redirectPath());
+        if (Auth::user()->hasRole('mahasiswa')) {
+            $Query = \Modules\V1\Entities\Pendaftaran::whereuserId(Auth::user()->id)->with("calon_mahasiswa")->first();
+            if ($Query->calon_mahasiswa == null) {
+                return redirect('/mahasiswa/form');
+            } else {
+                return redirect('/mahasiswa/profile');
+            }
+        } else if (Auth::user()->hasRole('admin')) {
+            return redirect($this->redirectPath());
+        }
     }
     public function controlLogout()
+    {
+        Auth::logout();
+    }
+    protected function  ()
     {
         Auth::logout();
     }
@@ -54,7 +66,7 @@ class LoginController extends Controller
         return [
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth("api")->factory()->getTTL() * 60
+            'expires_in' => 1000000000000
         ];
     }
 }
