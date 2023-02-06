cb4a9bb2403c0b00405709a7992413dc0c13b72f
egooktafanda97 <egooktafanda1097@gmail.com>
2023-02-07 03:53:23 +0700

    publish 2.5

--- system/resources/views/mahasiswa/page/Prodi/index.blade.php
+++ system/resources/views/mahasiswa/page/Prodi/index.blade.php
@@ -28,7 +28,7 @@
                                     <td scope="col">{{ $item->nama_prodi ?? '' }}</td>
                                     <td scope="col">{{ $item->akreditas ?? '' }}</td>
                                     <td scope="col">{{ $item->gelar ?? '' }}</td>
-                                    <td scope="col">{{ $item->biaya_kuliah ?? '' }}</td>
+                                    <td scope="col">{{ $item->biaya ?? '' }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
