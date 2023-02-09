922adbb659ba2ef81a6cd6fe473f72f4b77b9d95
egooktafanda97 <egooktafanda1097@gmail.com>
2023-02-09 09:57:20 +0700

    v1 publish 1.3

--- system/app/Http/Controllers/Api/mahasiswa/DaftarMhsController.php
+++ system/app/Http/Controllers/Api/mahasiswa/DaftarMhsController.php
@@ -224,47 +224,47 @@ class DaftarMhsController extends Controller
     }
     public function register(Request $request)
     {
-        try {
-            $resources = new ManagementCrud(str_replace('Controller', '', "CalonMahasiswaController"));
-            $pathJson =  ManagementServiceProvider::getScemaPath();
-            $resources->instance($pathJson);
-            $resources->setNameSpaceModel("\Modules\V1\Entities\\");
+        // try {
+        $resources = new ManagementCrud(str_replace('Controller', '', "CalonMahasiswaController"));
+        $pathJson =  ManagementServiceProvider::getScemaPath();
+        $resources->instance($pathJson);
+        $resources->setNameSpaceModel("\Modules\V1\Entities\\");
 
-            $Query = \Modules\V1\Entities\CalonMahasiswa::wherependaftaranId($request->pendaftaran_id)->with("orangtua")->first();
-            // $__ortu = \Modules\V1\Entities\OrangTua::where("calon_mahasiswa_id", $Query->id)->first();
+        $Query = \Modules\V1\Entities\CalonMahasiswa::wherependaftaranId($request->pendaftaran_id)->with("orangtua")->first();
+        // $__ortu = \Modules\V1\Entities\OrangTua::where("calon_mahasiswa_id", $Query->id)->first();
 
-            if (empty($Query)) {
-                /*
+        if (empty($Query)) {
+            /*
                 | MANAGEMENT CONTROL CALON MAHASISWA
                 */
-                $calon_mhs =  $resources->generate_data_insert($request);
-                /*
+            $calon_mhs =  $resources->generate_data_insert($request);
+            /*
                 | end
                 */
-                /*
+            /*
                 | MANAGEMENT CONTROL CALON ORANGTUA
                 */
-                if (!empty($calon_mhs['data']))
-                    $this->ortu($request->merge(["calon_mahasiswa_id" => $calon_mhs['data']['id']]));
-                /*
+            if (!empty($calon_mhs['data']))
+                $this->ortu($request->merge(["calon_mahasiswa_id" => $calon_mhs['data']['id']]));
+            /*
                 | end
                 */
-                return response()->json($calon_mhs, $calon_mhs["status"] ?? 400);
-            } else {
-                /*
+            return response()->json($calon_mhs, $calon_mhs["status"] ?? 400);
+        } else {
+            /*
                 | UPDATE MANAGEMENT CONTROL CALON MAHASISWA
                 */
-                $calon_mhs_update =  $resources->generate_data_update($request, $Query->id);
-                $id_ortu = !empty($Query->orangtua) ? $Query->orangtua->id : null;
-                $this->ortu($request->merge(["calon_mahasiswa_id" => $Query->id]), $id_ortu);
-                /*
+            $calon_mhs_update =  $resources->generate_data_update($request, $Query->id);
+            $id_ortu = !empty($Query->orangtua) ? $Query->orangtua->id : null;
+            $this->ortu($request->merge(["calon_mahasiswa_id" => $Query->id]), $id_ortu);
+            /*
                 | end
                 */
-                return response()->json($calon_mhs_update, $calon_mhs_update["status"] ?? 400);
-            }
-        } catch (\Throwable $th) {
-            return response()->json([], 500);
+            return response()->json($calon_mhs_update, $calon_mhs_update["status"] ?? 400);
         }
+        // } catch (\Throwable $th) {
+        //     return response()->json([], 500);
+        // }
     }
     public function upload_bukti_biaya_pendaftaran(Request $request)
     {
